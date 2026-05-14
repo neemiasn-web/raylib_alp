ec0055492b8bd137f736df8b1a006b21817d1815
neemiasn-web <neemias.n@aluno.senai.br>
2026-05-14 11:39:09 -0300

    Update main_Neemias.c
    Professor corrija minha atividade

--- main_Neemias.c
+++ main_Neemias.c
@@ -1,1 +1,22 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 600, "Meu Primeiro Game");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Bem-vindo ao Raylib", 10, 10, 40, BLUE);
+        DrawText("Pressione esc para sair", 10, 10, 40, BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
